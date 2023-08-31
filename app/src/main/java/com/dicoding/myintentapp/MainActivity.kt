55d2eaf8d8c2b5abda7cc445cb0ac94946497e86
Ilham Ramadhan <ilhamram332@gmail.com>
2023-08-31 19:26:10 +0700

    Latihan Implicit Intent

--- app/src/main/java/com/dicoding/myintentapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/myintentapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dicoding.myintentapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -19,6 +20,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
         btnMoveWithObject.setOnClickListener (this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener (this)
     }
 
     override fun onClick(v: View?) {
@@ -44,6 +48,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
                 startActivity(moveWithObjectIntent)
+            }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "08132880848"
+                val dialNumberIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialNumberIntent)
             }
         }
     }
